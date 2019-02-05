08b3b245b605fdb68f45e5c0fd5cbaac49839593
Mark Cooper <markchristophercooper@gmail.com>
2019-02-05 15:50:15 -0800

    Update routes

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,5 +1,6 @@
 ArchivesSpace::Application.routes.draw do
   scope AppConfig[:frontend_proxy_prefix] do
+    match('/plugins/shelve_it' => 'shelve_it#index', :via => [:get])
     match('/plugins/shelve_it/update' => 'shelve_it#update', :via => [:post])
   end
 end
