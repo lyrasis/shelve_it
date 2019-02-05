31483290aefa97b6b3194e6f018e81f318593859
Mark Cooper <markchristophercooper@gmail.com>
2019-02-05 15:49:49 -0800

    Add shelve it endpoint

--- backend/controllers/shelve_it_controller.rb
+++ backend/controllers/shelve_it_controller.rb
@@ -14,6 +14,23 @@ class ArchivesSpaceService < Sinatra::Base
     json_response(resolve_references(json, params[:resolve]))
   end
 
+  Endpoint.get('/repositories/:repo_id/shelve_it/assignments')
+    .description("Get recent container location assignments")
+    .params(
+      ["repo_id", :repo_id],
+      [
+        "limit",
+        Integer,
+        'Limit number of records returned',
+        :optional => true,
+        :default => 3,
+      ])
+    .permissions([])
+    .returns([200, "(:array)"]) \
+  do
+    json_response(shelve_it_assignments(limit: params[:limit]))
+  end
+
   Endpoint.get('/repositories/:repo_id/top_containers/barcodes/:barcode')
     .description("Get a top container by barcode")
     .params(["barcode", String],
@@ -33,6 +50,34 @@ class ArchivesSpaceService < Sinatra::Base
   # TODO: add to model
   def location_barcode(barcode)
     Location[:barcode => barcode]
+  end
+
+  def shelve_it_assignments(limit: 3)
+    result = []
+    DB.open do |db|
+      result += db[:location]
+        .join(
+          :top_container_housed_at_rlshp,
+          :top_container_housed_at_rlshp__location_id => :location__id
+        )
+        .join(
+          :top_container,
+          :top_container__id => :top_container_housed_at_rlshp__top_container_id
+        )
+        .order(Sequel.desc(:top_container_housed_at_rlshp__user_mtime))
+        .limit(limit)
+        .select(
+          Sequel.as(:location__barcode, :location_barcode),
+          Sequel.as(:location__id, :location_id),
+          Sequel.as(:location__title, :location_title),
+          Sequel.as(:top_container__barcode, :top_container_barcode),
+          Sequel.as(:top_container_housed_at_rlshp__status, :status),
+          Sequel.as(:top_container_housed_at_rlshp__user_mtime, :updated_at),
+          Sequel.as(:top_container__id, :top_container_id),
+        )
+        .all
+    end
+    result
   end
 
 end
