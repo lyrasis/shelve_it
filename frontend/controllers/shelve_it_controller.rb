e70bb59d6067127235ca9e070649d6b7c5163ce6
Mark Cooper <markchristophercooper@gmail.com>
2019-02-04 16:40:59 -0800

    Implement assignment in frontend

--- frontend/controllers/shelve_it_controller.rb
+++ frontend/controllers/shelve_it_controller.rb
@@ -8,25 +8,46 @@ class ShelveItController < ApplicationController
     container_barcode = params[:container_barcode]
     location_barcode  = params[:location_barcode]
 
-    container_uri = container_barcode # see top_containers_controller#perform_search
-    location_uri  = location_barcode  #
-    location_data = { container_uri => location_uri }
+    container = container_by_barcode(container_barcode)
+    location  = location_by_barcode(location_barcode)
 
-    response = JSONModel::HTTP.post_json(URI(shelve_it_uri), location_data.to_json)
-    result   = ASUtils.json_parse(response.body) rescue nil
-
-    if response.code =~ /^(4)/
-      flash[:error] = (result || response.message)
-    elsif response.code =~ /^5/
-      flash[:error] = response.message
+    if container && location
+      shelve_it(container['uri'] => location['uri'])
     else
-      flash[:info] = result
+      bad = [container_barcode.strip, location_barcode.strip].compact.join(',')
+      flash[:error] = "Error, check barcodes exist! #{bad}"
     end
 
     redirect_to controller: :welcome, action: :index
   end
 
   private
+
+  def container_by_barcode(barcode)
+    repo_id = session[:repo_id]
+    path    = "/repositories/#{repo_id}/top_containers/barcodes/#{barcode}"
+    JSONModel::HTTP.get_json(path)
+  end
+
+  def location_by_barcode(barcode)
+    path = "/locations/barcodes/#{barcode}"
+    JSONModel::HTTP.get_json(path)
+  end
+
+  def shelve_it(data)
+    response = shelve_it_request(shelve_it_uri, data)
+    result   = ASUtils.json_parse(response.body) rescue nil
+
+    if response.code =~ /^(4|5)/
+      flash[:error]  = response.message
+    else
+      flash[:success] = "Ok!"
+    end
+  end
+
+  def shelve_it_request(uri, data)
+    JSONModel::HTTP.post_json(URI(uri), data.to_json)
+  end
 
   def shelve_it_uri
     repo_id     = session[:repo_id]
